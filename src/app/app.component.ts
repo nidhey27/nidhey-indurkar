5598bc9029cbea5e552b3269fa7ff8858c323d0f
nidhey27 <nidhey.indurkar@infracloud.io>
2023-03-24 23:32:35 +0530

    seo meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nidhey-indurkar';
+
+  constructor(private meta: Meta) {
+    this.meta.addTag({ name: 'description', 
+                   content: "Discover the stunning portfolio of Nidhey Indurkar - a talented web developer and designer with a passion for crafting beautiful and functional websites. Explore his latest projects and learn more about his creative process, design philosophy, and technical skills. Whether you're looking for inspiration, guidance, or collaboration, Nidhey's website is a must-visit destination for anyone interested in web development and design. Click now to explore his work and get inspired!" });
+  }
+
+  
+
 }
